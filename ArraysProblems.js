a6317cb4204f1c19875550bc0ea7bd4aa081eaf4
keylorjc1 <keylorjc1@hotmail.com>
2023-01-07 17:09:01 -0600

    added Array problems

---
+++ ArraysProblems.js
@@ -0,0 +1,17 @@
+// Write a simple JavaScript program to join all elements of the following array into a string
+// Sample array : myColor = ["Red", "Green", "White", "Black"];
+
+// Write a JavaScript program which accept a number as input and insert dashes 
+// (-) between each two even numbers. For example if you accept 025468 the output should be 
+// 0-254-6-8.
+
+// Write a JavaScript program to sort the items of an array.
+// Sample array : var arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+// Sample Output : -4,-3,1,2,3,5,6,7,8
+
+// Write a JavaScript program to find the most frequent item of an array.
+// Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+// Sample Output : a ( 5 times )
+
+// Write a JavaScript program which accept a string as input and swap the case of each character. 
+// For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
