b86ddb49968efa28f65234dd8e73b39d893651c8
keylorjc1 <106986264+keylorjc1@users.noreply.github.com>
2023-01-07 16:54:13 -0600

    Add files via upload

---
+++ Practica2.js
@@ -0,0 +1,52 @@
+const users = [
+  { name: "Keylor", lastName: "Jimenez", age: 19, favColor: "blue" },
+  { name: "Sharon", lastName: "Nunez", age: 13, favColor: "red" },
+  { name: "Melissa", lastName: "Carvajal", age: 20, favColor: "orange" },
+  { name: "Valeria", lastName: "Soto", age: 16, favColor: "brown" },
+  { name: '',lastName: "Lara", age: 11, favColor: "white" },
+];
+
+const users2 = [
+  { name: "Keylor", lastName: "Jimenez", mom: "Emily", dad: "Hugo" },
+  { name: "Sharon", lastName: "Nunez", mom: "Patricia", dad: "Omar" },
+  { name: "Melissa", lastName: "Carvajal", mom: "Cinthia", dad: "Cristian" },
+  { name: "Valeria", lastName: "Soto", mom: "Catherine", dad: "David" },
+  { name: "Karina", lastName: "Lara", mom: "Marielos", dad: "Anthony" },
+];
+
+const num = [1, 2, -1, 2, 3, 4, 1, 6];
+const num2 = [1, 3, 1, 2, 3, 4, 5, 7];
+const personas = ["Hola", "Hola", "Casa", "Casa", "Pamela", "Sharon"];
+const dias = ["Enero", "Febrero", "Enero", "Febrero", "Marzo"];
+
+//que mande a llamar el unico que tenga menos de 3 letras
+//que mande a llamar al unico que la edad sea menor de 11
+//que me diga si hay alguno vacio
+//que me diga si todos son mayores
+//si algun incluye la letra K, s y m
+
+// const minValue = Math.min(...num);
+// const minArray = num.filter(el => el === minValue);
+
+//remove uno con id repetido
+// const arr = [{id: 1, name: 'one'}, {id: 2, name: 'two'}, {id: 1, name: 'one'}]
+
+// const ids = arr.map(o => o.id)
+// const filtered = arr.filter(({id}, index) => !ids.includes(id, index + 1))
+
+// console.log(filtered)
+
+//remove uno con id
+// const fruits = [
+//   {id:1, name: 'Banana'},
+//   {id:2, name: 'Apple'},
+//   {id:3, name: 'Kiwi'}
+// ]
+
+// Remove Apple with id '2'
+// const newFruits = fruits.filter(fruit => fruit.id !== 2)
+// console.log(newFruits)
+
+
+
+
