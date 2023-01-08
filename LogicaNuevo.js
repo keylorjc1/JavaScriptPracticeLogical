5398f30ace8aa7ff95cb434529895755a98f988a
keylorjc1 <keylorjc1@hotmail.com>
2023-01-08 09:30:54 -0600

    Asnwered questions

---
+++ LogicaNuevo.js
@@ -0,0 +1,11 @@
+// Write a JavaScript function to check whether an `input` is an array or not
+//Write a JavaScript function to check whether an `input` is a string or not.
+//Write a JavaScript function that checks whether a passed string is palindrome or not?
+//Write a JavaScript function that returns a passed string with letters in alphabetical order..
+//Write a javaScript function to find the longest work
+//Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case.
+// Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string
+//Remover repetidos
+//Fizz buzz
+//write function que cuente las palabras repetidas
+//Invertir un texto
